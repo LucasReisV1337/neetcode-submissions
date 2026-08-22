46d6c069c9a26caf58daf1c731dbc3d8ca3754dd
Lucas Reis <86315092+LucasReisV1337@users.noreply.github.com>
2026-08-22 00:48:04 -0300

    Add: two-integer-sum - submission-4

---
+++ Data Structures & Algorithms/two-integer-sum/submission-4.js
@@ -0,0 +1,21 @@
+class Solution {
+    /**
+     * @param {number[]} nums
+     * @param {number} target
+     * @return {number[]}
+     */
+    twoSum(nums, target) {
+        let complemento = 0
+        let map = new Map()
+
+        for(let i = 0; i < nums.length; i++){
+            complemento = target - nums[i]
+
+            if(map.has(complemento)){
+                return [map.get(complemento), i]
+            }
+            map.set(nums[i], i)
+        }
+
+    }
+}
